bf077eec602b8116f5896fba1718f1542fe3b4ab
leonid <mr.lenyaxxx@gmail.com>
2020-07-06 15:07:59 +0300

    added args

--- encryptdecrypt/Main.java
+++ encryptdecrypt/Main.java
@@ -3,20 +3,20 @@ import java.util.Scanner;
 
 public class Main {
 
-    static String encryption(char[] text, int key) {
+    static String encryption(String data, int key) {
         StringBuilder str = new StringBuilder();
 
-        for (char ch : text) {
+        for (char ch : data.toCharArray()) {
             str.append((char) (ch + key));
         }
 
         return str.toString();
     }
 
-    static String decryption(char[] text, int key) {
+    static String decryption(String data, int key) {
         StringBuilder str = new StringBuilder();
 
-        for (char ch : text) {
+        for (char ch : data.toCharArray()) {
             str.append((char) (ch - key));
         }
 
@@ -24,16 +24,25 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        String mode = scanner.nextLine();
-        char[] text = scanner.nextLine().toCharArray();
-        int key = scanner.nextInt();
+        String mode = "enc";
+        String data = "";
+        int key = 0;
+
+        for (int i = 0; i < args.length - 1; i += 2) {
+            if ("-mode".equals(args[i])) {
+                mode = args[i + 1];
+            } else if ("-key".equals(args[i])) {
+                key = Integer.parseInt(args[i + 1]);
+            } else if ("-data".equals(args[i])) {
+                data = args[i + 1];
+            }
+        }
         
         if ("enc".equals(mode)) {
-            System.out.println(encryption(text, key));
+            System.out.println(encryption(data, key));
         } else if ("dec".equals(mode)) {
-            System.out.println(decryption(text, key));
+            System.out.println(decryption(data, key));
         }
     }
 }
